7fc0223cfe17a07a85d7c7b6f4d84548917d9836
Roxie-32 <fizzyflexy397@gmail.com>
2021-06-28 18:35:23 +0100

    Google Two Factor Authentication Set

---
+++ app/Models/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'google2fa_secret'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'google2fa_secret'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setGoogle2faSecretAttribute($value)
+    {
+         $this->attributes['google2fa_secret'] = encrypt($value);
+    }
+
+    public function getGoogle2faSecretAttribute($value)
+    {
+        return decrypt($value);
+    }
+}
---
+++ resources/views/google2fa/register.blade.php
@@ -0,0 +1,25 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Set up Google Authenticator</div>
+
+                <div class="panel-body" style="text-align: center;">
+                    <p>Set up your two factor authentication by scanning the barcode below. Alternatively, you can use the code {{ $secret }}</p>
+                    <div>
+                        <img src="{{ $QR_Image }}">
+                    </div>
+                    <p>You must set up your Google Authenticator app before continuing. You will be unable to login otherwise</p>
+                    <div>
+                        <a href="/complete-registration"><button class="btn-primary">Complete Registration</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
